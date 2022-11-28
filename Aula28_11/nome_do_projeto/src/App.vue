0548998ada69292b5cfa98608ceba301935ed2d7
Junior <adilson.juniorcomunicacao@gmail.com>
2022-11-28 18:32:24 -0300

    comite28_11

---
+++ Aula28_11/nome_do_projeto/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <ContadorClicks/>
+    </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue';
+import ContadorClicks from './components/ContadorCliks.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ContadorClicks
+  },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Aula28_11/nome_do_projeto/src/components/ContadorCliks.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Contador de cliques: {{ contador }}</p>
+        <p><button @click="soma">somar</button></p>
+    </div>
+
+</template>
+
+
+<script>
+export default{
+    name: "ContadorClicks",
+    data() {
+        return {
+            contador: 0,
+        };
+    },
+    methods: {
+        soma() {
+            this.contador++;
+        },
+    },
+};
+</script>
