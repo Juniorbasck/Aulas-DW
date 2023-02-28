b7dc0a9735a4bb798b46ff081d5979521ed66ff6
Junior <adilson.juniorcomunicacao@gmail.com>
2023-02-28 17:48:29 -0300

    aula28_08

---
+++ Desenvolvimento web II/Aula28_02/praticando sala de aula/destino.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+    <meta charset="utf-8" />
+		<title>Praticando formulario</title>
+
+		<!-- Bootstrap início -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- Bootstrap fim -->
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    
+    <link rel="stylesheet" href="estilo.css">
+
+    <link src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.min.js">
+
+	</head>
+  <body>
+
+    <h1>Destino</h1>
+
+    <?php
+      $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+      $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL); 
+      $cor = filter_input(INPUT_POST, "cor", FILTER_SANITIZE_SPECIAL_CHARS); 
+
+
+      echo "<p>Nome informado: $nome<br>Email: $email<p/>"
+    ?>
+
+    <br>
+
+    <p>
+        <a href="form.html">Voltar para o formulario</a>
+    </p>
+
+    <style>
+      body{
+        background-color: <?php echo $cor; ?>;
+      }
+      
+    </style> 
+  </body>   	
+</html>
