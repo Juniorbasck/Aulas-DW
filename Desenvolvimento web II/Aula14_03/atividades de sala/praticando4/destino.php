3306980d768e13b7f1760ced98bb54dabf0d09e7
Junior <adilson.juniorcomunicacao@gmail.com>
2023-03-17 18:01:54 -0300

    atualizacoes

--- Desenvolvimento web II/Aula14_03/atividades de sala/praticando4/destino.php
+++ Desenvolvimento web II/Aula14_03/atividades de sala/praticando4/destino.php
@@ -7,34 +7,50 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <style>
-        
-    
-         .container input{
-                width: 200px;
+
+            *{
+                margin: 10px;
             }
-        
+            
+
         </style> 
 	</head>
   <body>
         
-        <h3>Parâmetros informados</h3>
+        <h3>Praticando 4 - Gerador de tabela</h3>
+        <hr>
 
+        
         <?php
             
             $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_SPECIAL_CHARS);
             $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_SPECIAL_CHARS);
-            $incremento = filter_input(INPUT_POST, "num3", FILTER_SANITIZE_SPECIAL_CHARS);
+            $estilo = filter_input(INPUT_POST, "estilo", FILTER_SANITIZE_SPECIAL_CHARS);
             
         ?>
 
+        <h4>Tabela <?php echo $num1 ?>x<?php echo $num2 ?></h4>
+        
+        <h4> <?php echo $estilo ?> </h4>
 
-        <?php 
-            
-           
-     
-         
-        ?>
-     
+        <Table class="table  <?php echo $estilo ?>">
+
+            <?php
+
+                for ($i = 0; $i < $num1; $i++) {
+
+                    echo "<tr>";
+                        for ($j = 0; $j < $num2; $j++) {
+                            echo "<td>-","</td>";
+                        }
+                        
+                    echo "</tr>";
+                }
+              
+            ?>
+        </Table>
+
+       
 
   </body>   	
 </html>
